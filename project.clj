fc3b740ee8effbe46ecc8b494f9eee4101a2971c
Jan Ruusuvuori <jan.ruusuvuori@weave.fi>
2017-09-14 09:30:13 +0300

    set up deploy to private maven repo

--- project.clj
+++ project.clj
@@ -10,5 +10,17 @@
                  [clj-time "0.14.0"]
                  [yleisradio/http-kit "2.2.0-sni-support"]]
   :profiles {:dev {:dependencies [[speclj "3.3.2"]]}}
-  :plugins [[speclj "3.3.2"]]
+  :plugins [[fi.yle.tools/aws-maven "1.4.0"]
+            [speclj "3.3.2"]]
+  :repositories [["yle-public" "http://maven.c4.yle.fi/release"]
+                 ["releases" {:url           "s3://yle-maven-private/release"
+                              :sign-releases false
+                              :snapshots     false
+                              :username      ""
+                              :password      ""}]
+                 ["snapshots" {:url           "s3://yle-maven-private/snapshot"
+                               :sign-releases false
+                               :snapshots     true
+                               :username      ""
+                               :password      ""}]]
   :test-paths ["spec"])
