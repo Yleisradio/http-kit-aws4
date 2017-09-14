a8feda6229c74b84d6df025039d46c9617e760ff
Jan Ruusuvuori <jan.ruusuvuori@weave.fi>
2017-09-14 09:24:41 +0300

    set project description, url and license (MIT)

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject http-kit-aws4 "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :description "AWS Request Signing v4 for http-kit"
+  :url "https://github.com/Yleisradio/http-kit-aws4"
+  :license {:name "MIT License"
+            :url "http://www.opensource.org/licenses/mit-license.php"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [buddy "1.3.0"]
                  [camel-snake-kebab "0.4.0"]
