741d7a4b37e67f2eb7051312f396fb732b8010de
Jan Ruusuvuori <jan.ruusuvuori@kodan.fi>
2020-09-23 12:09:19 +0300

    version 0.3.0-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject http-kit-aws4 "0.2.0"
+(defproject http-kit-aws4 "0.3.0-SNAPSHOT"
   :description "AWS Request Signing v4 for http-kit"
   :url "https://github.com/Yleisradio/http-kit-aws4"
   :license {:name "MIT License"
